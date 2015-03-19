4795d33511c9319b0c15d76de58e051d225844f1
cwhite911 <smortopahri@gmail.com>
2015-03-19 11:55:54 -0400

    Init commit with yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hydrantsDashboardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hydrantsDashboardApp
+ */
+angular.module('hydrantsDashboardApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hydrantsDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hydrantsDashboardApp
+ */
+angular.module('hydrantsDashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
