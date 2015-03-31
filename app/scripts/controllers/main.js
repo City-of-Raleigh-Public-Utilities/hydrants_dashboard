bfe9489eec32da9dd2c4d3fc66490a35be870ad2
cwhite911 <smortopahri@gmail.com>
2015-03-31 13:41:05 -0400

    added scroll to on main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,22 @@
  * Controller of the hydrantsDashboardApp
  */
 angular.module('hydrantsDashboard')
-  .controller('MainCtrl',['$scope', 'FIREDEPTS', function ($scope, FIREDEPTS) {
+  .controller('MainCtrl',['$anchorScroll', '$location', '$scope', 'FIREDEPTS', function ($anchorScroll, $location, $scope, FIREDEPTS) {
     $scope.departments = FIREDEPTS;
+
+
+    $scope.gotoAnchor = function() {
+      var newHash = 'stations';
+      if ($location.hash() !== newHash) {
+        // set the $location.hash to `newHash` and
+        // $anchorScroll will automatically scroll to it
+        $location.hash('stations');
+      } else {
+        // call $anchorScroll() explicitly,
+        // since $location.hash hasn't changed
+        $anchorScroll();
+      }
+    };
+
+
   }]);
